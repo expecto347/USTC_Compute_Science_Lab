d8f542c7a2d202f5026dff0b7ea65851c2c7b1cd
Macbook <expecto@mail.ustc.edu.cn>
2022-05-25 17:18:14 +0800

    debug

--- OS_Lab/Lab4/userApp/memTestCase.c
+++ OS_Lab/Lab4/userApp/memTestCase.c
@@ -313,11 +313,11 @@ int user_malloc(int argc, unsigned char **argv){
 	for(;n!=0;n--){
 		size_1 = (unsigned long)(argv[1][length-n] - 48);
 		for(i=n;i!=0;i--){
-			size_1 = size_1 * 10;
+			size_1 = size_1 * 16;
 		}
 		size = size + size_1;
 	}
-	myPrintf(0x7,"%d\n",size);
+	myPrintf(0x7,"%x\n",size);
 	tmp_handler = malloc(size);
 	if(tmp_handler) {
 		myPrintf(0x7,"Suceessful! Address = 0x%x\n", tmp_handler);
