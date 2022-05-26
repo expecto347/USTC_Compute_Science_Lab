9fcd15efc9ea528a729831c2650319dff7bf870a
expecto <you@example.com>
2022-05-26 23:01:59 +0800

    ee

---
+++ COD_Lab/Lab4/one_circle_CPU/one_circle_CPU.ip_user_files/ip/ROM_Instruction/ROM_Instruction_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
+// Date        : Thu Apr 21 19:44:18 2022
+// Host        : expecto running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top ROM_Instruction -prefix
+//               ROM_Instruction_ ROM_Instruction_stub.v
+// Design      : ROM_Instruction
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a100tcsg324-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "dist_mem_gen_v8_0_13,Vivado 2019.1" *)
+module ROM_Instruction(a, spo)
+/* synthesis syn_black_box black_box_pad_pin="a[7:0],spo[31:0]" */;
+  input [7:0]a;
+  output [31:0]spo;
+endmodule
