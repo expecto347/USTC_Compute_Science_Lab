1c238ce247eddcbd462cdfad12735f05a6397f8f
Macbook <expecto@mail.ustc.edu.cn>
2022-06-14 19:38:58 +0800

    更新了make机制

--- OS_Lab/Lab5-6/source2img.sh
+++ OS_Lab/Lab5-6/source2img.sh
@@ -8,5 +8,7 @@ if [ $? -ne 0 ]; then
 	echo "make failed"
 else
 	echo "make succeed"
-	qemu-system-i386 -kernel output/myOS.elf -serial pty -S -s&
+	# qemu-system-i386 -kernel output/myOS.elf -serial pty -S -s&
+	git add .
+	git commit -m "bot_auto_commit"
 fi
