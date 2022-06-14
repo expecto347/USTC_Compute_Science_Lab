6ade4e19c122c96f176216e4c50767d6ac362483
Macbook <expecto@mail.ustc.edu.cn>
2022-06-15 01:03:58 +0800

    mac-auto-push

--- OS_Lab/Lab5-6/Shellscript/pull.sh
+++ OS_Lab/Lab5-6/Shellscript/pull.sh
@@ -1,9 +1,8 @@
-gdb=1 #是否调试
+g=1 #是否调试
 
 git pull
 
-if gdb
-then
+if $g
     qemu-system-i386 -kernel output/myOS.elf -serial pty -s -S&
 else
     qemu-system-i386 -kernel output/myOS.elf -serial pty&
