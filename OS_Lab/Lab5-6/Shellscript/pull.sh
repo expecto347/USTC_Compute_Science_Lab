c7b16bfee2454f407bfe48841c7f85a14a4f0daf
Macbook <expecto@mail.ustc.edu.cn>
2022-06-15 01:06:40 +0800

    mac-auto-push

--- OS_Lab/Lab5-6/Shellscript/pull.sh
+++ OS_Lab/Lab5-6/Shellscript/pull.sh
@@ -6,6 +6,5 @@ if $g
 then
     qemu-system-i386 -kernel output/myOS.elf -serial pty -s -S&
 else
-    qemu-system-i386 -kernel output/myOS.elf -seri
-    al pty&
+    qemu-system-i386 -kernel output/myOS.elf -serial pty&
 fi
