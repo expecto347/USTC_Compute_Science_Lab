b92f2764a83f774531175dcc09ec45bcb68cf354
Macbook <expecto@mail.ustc.edu.cn>
2022-06-16 02:08:18 +0800

    mac-auto-push

--- OS_Lab/Lab5-6/userApp/main.c
+++ OS_Lab/Lab5-6/userApp/main.c
@@ -3,8 +3,7 @@
 #include "shell.h"
 #include "memTestCase.h"
 
-tskPara *myMain_tskPara, *Task_1_tskPara, *Task_2_tskPara, *Task_3_tskPara;
-
+tskPara *myMain_tskPara, *Task_1_tskPara, *Task_2_tskPara, *Task_3_tskPara, *Task_4_tskPara, *Task_5_tskPara;
 void wallClock_hook_main(void){
 	int _h, _m, _s;
 	char hhmmss[]="hh:mm:ss\0\0\0\0";
@@ -63,15 +62,38 @@ void Task_3(void){
 	return;
 }
 
+void Task_4(void){
+	int color;
+	color = 0x4;
+	myPrintf(color,"*********************************************************\n");
+	myPrintf(color,"****************This is the Task_4 task!*****************\n");
+	walk_tskPara(Task_4_tskPara,color);
+	myPrintf(color,"*********************************************************\n");
+	myPrintf(color,"\n");
+	return;
+}
+
+void Task_5(void){
+	int color;
+	color = 0x5;
+	myPrintf(color,"*********************************************************\n");
+	myPrintf(color,"****************This is the Task_5 task!*****************\n");
+	walk_tskPara(Task_5_tskPara,color);
+	myPrintf(color,"*********************************************************\n");
+	myPrintf(color,"\n");
+	return;
+}
 
 void scheduleInit(void){
 	time = 0;
-	int myMain_tid, Task_1_tid, Task_2_tid, Task_3_tid;
+	int myMain_tid, Task_1_tid, Task_2_tid, Task_3_tid, Task_4_tid, Task_5_tid;
 
 	myMain_tskPara = (tskPara*)malloc(sizeof(tskPara));
 	Task_1_tskPara = (tskPara*)malloc(sizeof(tskPara));
 	Task_2_tskPara = (tskPara*)malloc(sizeof(tskPara));
 	Task_3_tskPara = (tskPara*)malloc(sizeof(tskPara));
+	Task_4_tskPara = (tskPara*)malloc(sizeof(tskPara));
+	Task_5_tskPara = (tskPara*)malloc(sizeof(tskPara));
 	//为taskPara分配空间
 
 	{
@@ -98,10 +120,24 @@ void scheduleInit(void){
 		Task_3_tskPara->priority = 3;
 	}
 
+	{
+		Task_4_tskPara->arrTime = 20;
+		Task_4_tskPara->exeTime = 20;
+		Task_4_tskPara->priority = 4;
+	}
+
+	{
+		Task_5_tskPara->arrTime = 20;
+		Task_5_tskPara->exeTime = 10;
+		Task_5_tskPara->priority = 5;
+	}
+
 	myMain_tid = createTsk(myMain,myMain_tskPara); //创建myMain任务
 	Task_1_tid = createTsk(Task_1,Task_1_tskPara); //创建Task_1任务
 	Task_2_tid = createTsk(Task_2,Task_2_tskPara); //创建Task_2任务
 	Task_3_tid = createTsk(Task_3,Task_3_tskPara); //创建Task_3任务
+	Task_4_tid = createTsk(Task_4,Task_4_tskPara); //创建Task_4任务
+	Task_5_tid = createTsk(Task_5,Task_5_tskPara); //创建Task_5任务
 
 	//依次添加任务到就绪队列
 	while (1) schedule(); //进入调度程序
