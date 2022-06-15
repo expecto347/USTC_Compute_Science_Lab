8d55cb89d7d6ec80d67ad30b5807bd1cf55586e7
Macbook <expecto@mail.ustc.edu.cn>
2022-06-15 20:13:11 +0800

    mac-auto-push

--- OS_Lab/Lab5-6/myOS/include/tick.h
+++ OS_Lab/Lab5-6/myOS/include/tick.h
@@ -1,6 +1,6 @@
 #ifndef __TICK_H__
 #define __TICK_H__
 
-extern void (*tick_hook)(void) = 0;
+void (*tick_hook)(void) = 0;
 
 #endif
--- OS_Lab/Lab5-6/myOS/kernel/tick.c
+++ OS_Lab/Lab5-6/myOS/kernel/tick.c
@@ -2,10 +2,10 @@
 
 extern void oneTickUpdateWallClock(void);       //TODO: to be generalized
 
-//#ifndef __TICK_HOOK__
-//#define __TICK_HOOK__
+#ifndef __TICK_HOOK__
+#define __TICK_HOOK__
 void (*tick_hook)(void) = 0; //{};
-//#endif
+#endif
 
 int tick_number = 0;
 void tick(void){
