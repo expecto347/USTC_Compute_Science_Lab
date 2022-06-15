8eb279c0977b0f145f98dd41448ed7dc6a0936ca
Macbook <expecto@mail.ustc.edu.cn>
2022-06-16 00:51:57 +0800

    mac-auto-push

--- OS_Lab/Lab5-6/myOS/kernel/task/schedule/SJF.c
+++ OS_Lab/Lab5-6/myOS/kernel/task/schedule/SJF.c
@@ -22,6 +22,8 @@ void SJF_schedule(void){
     if(tsk->tid == 0 && currentTsk->tid == 0) return; //如果当前任务以及下一个任务都是idle任务，则不做任何操作
     else{
         currentTsk = tsk;
+        rdyQ->idleTsk->stack_top = rdyQ->idleTsk->stack_base;
+        stack_init(&rdyQ->idleTsk->stack_top,idleTsk_func);
         context_switch(&preTskStackPointer, tsk->stack_top); //上下文切换，将当前任务的栈指针存储在preTskStackPointer中，将下一个任务的栈指针传入，进行上下文切换
     }
 }
